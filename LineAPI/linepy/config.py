44d1c77deb42d7cf961763c64884a83e5a0052e1
ARIF MH <37590868+arifistifik@users.noreply.github.com>
2018-08-09 07:07:07 +0000

    Update config.py

--- LineAPI/linepy/config.py
+++ LineAPI/linepy/config.py
@@ -29,7 +29,7 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.4.1'
+    APP_VER     = '0.0.9'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'ARIF-PC'
     SYSTEM_VER  = '8.22.17'
